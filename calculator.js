c6e52f95cbe2c02f9d92ea65b52c348782fd9514
Marta Arcones <marta.arconesrodriguez@zooplus.com>
2024-11-19 19:25:04 +0100

    Enhance how results are shown

--- calculator.js
+++ calculator.js
@@ -6,7 +6,7 @@ program
   .description('Suma dos números enteros')
   .action((numero1, numero2) => {
     const result = parseInt(numero1, 10) + parseInt(numero2, 10);
-    console.log(`El resultado es ${result}.`);
+    console.log(`==>  ${result} <==`);
   });
 
 program.parse(process.argv);
