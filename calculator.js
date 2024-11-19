7b2f8ae8fa316f5c65f6ba23f2c258e4ecf59961
Marta Arcones <marta.arconesrodriguez@zooplus.com>
2024-11-19 19:19:03 +0100

    Add subtraction operation

--- calculator.js
+++ calculator.js
@@ -9,5 +9,13 @@ program
     console.log(`El resultado es: ${result}`);
   });
 
+program
+  .command('restar numero1 numero2')
+  .description('Resta dos números enteros')
+  .action((numero1, numero2) => {
+    const result = parseInt(numero1, 10) - parseInt(numero2, 10);
+    console.log(`El resultado es: ${result}`);
+  });
+
 program.parse(process.argv);
 
