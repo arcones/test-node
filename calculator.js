9352f9f28aba900c4250260132101ab6fde5e30c
Marta Arcones <marta.arconesrodriguez@zooplus.com>
2024-11-20 18:40:20 +0100

    Add multiplication. Enhance output

--- calculator.js
+++ calculator.js
@@ -6,7 +6,7 @@ program
   .description('Suma dos números enteros')
   .action((numero1, numero2) => {
     const result = parseInt(numero1, 10) + parseInt(numero2, 10);
-    console.log(`==>  ${result} <==`);
+    console.log(`=>  ${result} <=`);
   });
 
 program
@@ -14,7 +14,15 @@ program
   .description('Resta dos números enteros')
   .action((numero1, numero2) => {
     const result = parseInt(numero1, 10) - parseInt(numero2, 10);
-    console.log(`==>  ${result} <==`);
+    console.log(`=>  ${result} <=`);
+  });
+
+program
+  .command('multiplicar numero1 numero2')
+  .description('Multiplica dos números enteros')
+  .action((numero1, numero2) => {
+    const result = parseInt(numero1, 10) * parseInt(numero2, 10);
+    console.log(`=>  ${result} <=`);
   });
 
 program.parse(process.argv);
