e927297e2c0eaf33f285cf6c2041da31d6842e81
Marta Arcones <marta.arconesrodriguez@zooplus.com>
2024-11-23 12:41:16 +0100

    Allow float operations

--- calculator.js
+++ calculator.js
@@ -3,25 +3,25 @@ const program = new Command();
 
 program
   .command('sumar numero1 numero2')
-  .description('Suma dos números enteros')
+  .description('Suma dos números ')
   .action((numero1, numero2) => {
-    const result = parseInt(numero1, 10) + parseInt(numero2, 10);
+    const result = parseFloat(numero1) + parseFloat(numero2);
     console.log(`=>  ${result} <=`);
   });
 
 program
   .command('restar numero1 numero2')
-  .description('Resta dos números enteros')
+  .description('Resta dos números')
   .action((numero1, numero2) => {
-    const result = parseInt(numero1, 10) - parseInt(numero2, 10);
+    const result = parseFloat(numero1) - parseFloat(numero2);
     console.log(`=>  ${result} <=`);
   });
 
 program
   .command('multiplicar numero1 numero2')
-  .description('Multiplica dos números enteros')
+  .description('Multiplica dos números')
   .action((numero1, numero2) => {
-    const result = parseInt(numero1, 10) * parseInt(numero2, 10);
+    const result = parseFloat(numero1) * parseFloat(numero2);
     console.log(`=>  ${result} <=`);
   });
 
